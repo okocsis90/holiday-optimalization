95e1404bb11b63cb71b609c06d821d766606ee16
okocsis90 <o.kocsis90@gmail.com>
2017-09-28 14:13:58 +0200

    Created DestinationNode class. Also created Optimizer class, with a basic optimize method.

---
+++ HolidayOptimizer/HolidayOptimizer/DestinationNode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidayOptimizer
+{
+    public class DestinationNode
+    {
+        public string Name { get; set; }
+        public DestinationNode Previous { get; set; }
+
+        public DestinationNode(string name)
+        {
+            Name = name;
+            Previous = null;
+        }
+
+        public DestinationNode(string name, DestinationNode previous)
+        {
+            Name = name;
+            Previous = previous;
+        }
+    }
+}
---
+++ HolidayOptimizer/HolidayOptimizer/Optimizer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidayOptimizer
+{
+    public class Optimizer
+    {
+        public static List<DestinationNode> Optimize(List<DestinationNode> inputNodes)
+        {
+            if (inputNodes.Count == 0)
+            {
+                throw new ArgumentException("Input list is empty, please add Destinations to your list.");
+            }
+            bool inputIsEqualToOutput = true;
+            foreach (DestinationNode destination in inputNodes)
+            {
+                if (destination.Previous != null)
+                {
+                    inputIsEqualToOutput = false;
+                    break;
+                }
+            }
+            if (inputIsEqualToOutput)
+            {
+                return inputNodes;
+            }
+            List<DestinationNode> resultNodes = new List<DestinationNode>();
+            return resultNodes;
+        }
+    }
+}
