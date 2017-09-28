6a98f98c70a28b815b5590e32bd60a7ced0a54b9
okocsis90 <o.kocsis90@gmail.com>
2017-09-28 14:14:18 +0200

    Created first tests for optimizer class.

---
+++ HolidayOptimizer/HolidayOptimizerTests/OptimizerTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HolidayOptimizer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidayOptimizer.Tests
+{
+    [TestClass()]
+    public class OptimizerTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException),
+        "Input list is empty, please add Destinations to your list.")]
+        public void OptimizeTest_InputIsEmpty_ThrowsException()
+        {
+            Optimizer.Optimize(new List<DestinationNode>());
+        }
+
+        [TestMethod()]
+        public void OptimizeTest_InputHasNoDependencies_GivesBackInput()
+        {
+            List<DestinationNode> allLocations = new List<DestinationNode> {
+                new DestinationNode("Location A"),
+                new DestinationNode("Location B"),
+                new DestinationNode("Location C")
+        };
+            Assert.AreEqual(allLocations, Optimizer.Optimize(allLocations));
+
+
+        }
+    }
+}
